0a9a09e542b0e0dec3035faa61d629dd92ce7570
Jessica Wade <jessica.wade@parameter1.com>
2021-09-13 13:28:42 -0500

    Fix error on initial email-x file

--- tenants/all/config/email-x.js
+++ tenants/all/config/email-x.js
@@ -2,6 +2,4 @@ const EmailXConfiguration = require('@parameter1/base-cms-marko-newsletters-emai
 
 const config = new EmailXConfiguration(process.env.EMAILX_SERVE_URI || 'https://abmedia.serve.email-x.parameter1.com');
 
-config
-
 module.exports = config;
