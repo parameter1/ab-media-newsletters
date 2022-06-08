6da951c1fef7757ecf7a6c3312cb64b6cfa393cc
Jessica Wade <jessica.wade@parameter1.com>
2022-06-08 10:57:41 -0500

    Add ‘athletic business today’ to core.js

--- tenants/all/config/core.js
+++ tenants/all/config/core.js
@@ -1,16 +1,19 @@
-// const brands = require('./brands');
+const brands = require('./brands');
 const emailX = require('./email-x');
-const nativeX = require('./native-x');
+// const nativeX = require('./native-x');
 
 const config = {
   emailX,
-  nativeX,
+  // nativeX,
   dpm: {
     emailx: {
       enabled: true,
     },
   },
-
+  'athletic-business-today': {
+    name: 'Athletic Business Today',
+    ...brands.ab,
+  },
 };
 
 module.exports = config;
