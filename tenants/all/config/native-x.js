52a85cb343e5ac4dfe6ea698fa2cd4a79aa9a947
Jessica Wade <jessica.wade@parameter1.com>
2022-06-22 10:49:12 -0500

    Enable & Add nativeX slot

--- tenants/all/config/native-x.js
+++ tenants/all/config/native-x.js
@@ -1,6 +1,8 @@
-// module.exports = {
-//   uri: 'https://abmedia.native-x.parameter1.com',
-//   placements: {
-
-//   },
-// };
+module.exports = {
+  uri: 'https://abmedia.native-x.parameter1.com',
+  placements: {
+    'athletic-business-today': {
+      'slot-1': '62b2220b1c9be80001f4e117',
+    },
+  },
+};
